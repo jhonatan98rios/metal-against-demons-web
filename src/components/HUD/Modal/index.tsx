d9f13aeefe6fd4b03bcadac96ea7ac41eefb47f2
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-01-06 21:11:36 -0300

    typescript config fix

--- src/components/HUD/Modal/index.tsx
+++ src/components/HUD/Modal/index.tsx
@@ -7,7 +7,7 @@ interface IModal  {
 export function Modal({ children }: IModal) {
 
     return (
-        <div className="max-w-lg w-full h-96 bg-slate-600 m-auto absolute left-0 right-0 top-32 p-4">
+        <div className="max-w-lg w-11/12 h-96 bg-slate-600 m-auto absolute left-0 right-0 top-32 p-4">
             { children }
         </div>
     )
