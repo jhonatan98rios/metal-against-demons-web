278c3dfbf3173b81fb557f89561ecf5a7ea4e6e1
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-01-14 20:20:25 -0300

    bg test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import { CSSProperties } from "react";
 
 
 export default function Home() {
+
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-         <Link href="/play">
-            Jogar
-         </Link>
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24 main-bg w-screen`}>
+      <Link href="/play" className="border-red-500 text-center border-8 py-2 px-8 text-6xl mt-[30%] text-red-500 font-black rotate-12">
+        JOGAR
+      </Link>
     </main>
   )
 }
