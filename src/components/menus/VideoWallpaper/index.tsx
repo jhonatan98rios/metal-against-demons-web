53e9e5aadeff55b7793ca8a3e8f225d496a00492
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-01-20 02:13:39 -0300

    responsiveness fix

--- src/components/menus/VideoWallpaper/index.tsx
+++ src/components/menus/VideoWallpaper/index.tsx
@@ -1,14 +1,10 @@
-import Link from "next/link";
-import { CSSProperties } from "react";
-
-
 export default function VideoWallpaper() {
 
-  return (
-    <div className="video-bg absolute bottom-0 left-0 w-full h-full">
-    <video autoPlay muted loop id="bg" className="fixed z-0 bottom-0 left-0 min-w-full min-h-full">
-        <source src="./video/background.mp4" type="video/mp4" />
-    </video>
-    </div>
-  )
+    return (
+        <div className="video-bg absolute bottom-0 left-0 w-full h-full">
+            <video autoPlay muted loop id="bg" className="fixed z-0 bottom-0 left-0 h-screen max-w-max min-w-full min-h-full lg:h-auto">
+                <source src="./video/background.mp4" type="video/mp4" />
+            </video>
+        </div>
+    )
 }
