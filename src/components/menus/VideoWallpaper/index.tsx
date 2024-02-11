0904929921808246cf76784fae4804552257c6c5
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2024-02-11 00:55:06 -0300

    fixing video position

--- src/components/menus/VideoWallpaper/index.tsx
+++ src/components/menus/VideoWallpaper/index.tsx
@@ -8,11 +8,7 @@ export default function VideoWallpaper() {
         <div className="video-bg absolute bottom-0 left-0 w-full h-full flex justify-center items-center">
             <video 
                 autoPlay muted loop id="bg"
-                className={`
-                    fixed z-0 mx-auto video-zoom
-                    h-screen max-w-max min-w-full min-h-full lg:h-auto
-                    ${animationState.className}
-                `}
+                className={`fixed z-0 mx-auto video-zoom h-screen max-w-max min-w-full min-h-full lg:h-auto bottom-20 left-0 md:-bottom-20`}
             >
                 <source src="./video/mobile.mp4" type="video/mp4" className="hidden md:block" />
                 <source  src="./video/background-4k-resized.mp4" className="block md:hidden" />
